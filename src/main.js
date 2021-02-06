3f7c35b4880cf154dae99d4d2c0d9ddbbe1fb904
ahaoboy <504595380@qq.com>
2021-02-06 17:00:03 +0800

    add readme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// I use url to init a sharedworker, but also get import errorF 
-// import SW from "./sharedworker.js?url";
+
+// I use url to init a sharedworker, but also get import error
+/*
+import SW_URL from "./sharedworker.js?url";
+const sw = new SharedWorker(SW_URL);
+sw.port.start();
+document.addEventListener("click", function () {
+  sw.port.postMessage("sw");
+});
+sw.port.onmessage = function (val) {
+  document.body.innerHTML = val.data;
+};
+*/
 
 import Worker from "./worker?worker";
 const worker = new Worker();
